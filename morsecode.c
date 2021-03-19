47a9677d3940dd8b9c3e21a8f3ced2b02a1a9a10
Cameron Savage <cdsavage@sfu.ca>
2021-03-19 16:23:04 -0700

    filled in template for morsecode.c

--- morsecode.c
+++ morsecode.c
@@ -1,18 +1,50 @@
 #include <linux/module.h>
-//#include <linux/init.h>
+#include <linux/miscdevice.h>
 #include <linux/kernel.h>
+#include <linux/fs.h>
+
+#define MY_DEVICE_FILE "morse-codeS"
+
+static int my_open(struct inode *inode, struct file *file) {
+    return 0;
+}
+
+static int my_close(struct inode *inode, struct file *file) {
+    return 0;
+}
+
+static ssize_t my_read(struct file *file, char *buff, size_t count, loff_t *ppos) {
+    return 0;
+}
+
+static ssize_t my_write(struct file *file, const char *buff, size_t count, loff_t *ppos) {
+    return 0;
+}
+
 
 static int __init morsecode_init(void) {
     printk(KERN_INFO "----> My test driver init()\n");
     return 0; 
 }
 
-
-static void __exit morsecode_exit(void)
-{
+static void __exit morsecode_exit(void) {
       printk(KERN_INFO "<---- My test driver exit().\n");
 }
 
+
+static struct file_operations my_fops = {
+    .owner = THIS_MODULE,
+    .open = my_open,
+    .release = my_close, 
+    .read = my_read,
+    .write = my_write
+};
+
+static struct miscdevice my_miscdevice = { 
+    .minor = MISC_DYNAMIC_MINOR,
+    .name = MY_DEVICE_FILE,
+    .fops = &my_fops
+};
 
 // Link our init/exit functions into the kernel's code.
 module_init(morsecode_init);
